f8e7541cc54840fa28073f243f4cf9b67ab2620e
gaurav1246 <gauravverma@virtualemployee.com>
2024-06-12 20:54:28 +0530

    add floating action button

--- lib/app/modules/home/HomeView.dart
+++ lib/app/modules/home/HomeView.dart
@@ -2,6 +2,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:sportcommunityapp/app/theme/ProjectTheme.dart';
 import 'package:sportcommunityapp/util/AppStrings.dart';
 
 import '../../core/BaseView.dart';
@@ -16,111 +17,129 @@ class HomeView extends BaseView<HomeViewModel> {
   @override
   Widget body(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SearchBar(
-              //padding: EdgeInsetsGeometry.lerp(a, b, t)
-              leading: const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Icon(Icons.search_rounded),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SearchBar(
+                //padding: EdgeInsetsGeometry.lerp(a, b, t)
+                leading: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(Icons.search_rounded),
+                ),
+                onTap: () {
+                  //Go to the next screen
+                },
               ),
-              onTap: () {
-                //Go to the next screen
-              },
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                pendinginvite,
-                style: Theme.of(context).textTheme.titleLarge,
+              SizedBox(
+                height: 8,
               ),
-            ),
-            Obx(() => controller.pendingfriends.value.length <= 0
-                ? CarouselSlider(
-                    options: CarouselOptions(height: 100.0),
-                    items: [1, 2, 3, 4, 5].map((i) {
-                      return Builder(
-                        builder: (BuildContext context) {
-                          return Card.filled(
-                            elevation: 4,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                  width: MediaQuery.of(context).size.width,
-                                  // margin: EdgeInsets.symmetric(horizontal: 5.0),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  pendinginvite,
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ),
+              Obx(() => controller.pendingfriends.value.length <= 0
+                  ? CarouselSlider(
+                      options: CarouselOptions(height: 100.0),
+                      items: [1, 2, 3, 4, 5].map((i) {
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return Card.filled(
+                              elevation: 4,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                    width: MediaQuery.of(context).size.width,
+                                    // margin: EdgeInsets.symmetric(horizontal: 5.0),
 
-                                  child: Text(
-                                    'text $i',
-                                    style: TextStyle(fontSize: 16.0),
-                                  )),
-                            ),
-                          );
-                        },
+                                    child: Text(
+                                      'text $i',
+                                      style: TextStyle(fontSize: 16.0),
+                                    )),
+                              ),
+                            );
+                          },
+                        );
+                      }).toList(),
+                    )
+                  : CarouselSlider(
+                      options: CarouselOptions(height: 100.0),
+                      items: controller.pendingfriends.value.map((i) {
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return Card.filled(
+                              elevation: 4,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                    width: MediaQuery.of(context).size.width,
+                                    // margin: EdgeInsets.symmetric(horizontal: 5.0),
+
+                                    child: Text(
+                                      ' ${i.UserID}',
+                                      style: TextStyle(fontSize: 16.0),
+                                    )),
+                              ),
+                            );
+                          },
+                        );
+                      }).toList(),
+                    )),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  upcomingevent,
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ),
+              CarouselSlider(
+                options: CarouselOptions(height: 200.0),
+                items: [1, 2, 3, 4, 5].map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Card.filled(
+                        elevation: 4,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                              width: MediaQuery.of(context).size.width,
+                              // margin: EdgeInsets.symmetric(horizontal: 5.0),
+
+                              child: Text(
+                                'text $i',
+                                style: TextStyle(fontSize: 16.0),
+                              )),
+                        ),
                       );
-                    }).toList(),
-                  )
-                : CarouselSlider(
-                    options: CarouselOptions(height: 100.0),
-                    items: controller.pendingfriends.value.map((i) {
-                      return Builder(
-                        builder: (BuildContext context) {
-                          return Card.filled(
-                            elevation: 4,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                  width: MediaQuery.of(context).size.width,
-                                  // margin: EdgeInsets.symmetric(horizontal: 5.0),
-
-                                  child: Text(
-                                    ' ${i.UserID}',
-                                    style: TextStyle(fontSize: 16.0),
-                                  )),
-                            ),
-                          );
-                        },
-                      );
-                    }).toList(),
-                  )),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                upcomingevent,
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-            ),
-            CarouselSlider(
-              options: CarouselOptions(height: 200.0),
-              items: [1, 2, 3, 4, 5].map((i) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Card.filled(
-                      elevation: 4,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                            width: MediaQuery.of(context).size.width,
-                            // margin: EdgeInsets.symmetric(horizontal: 5.0),
-
-                            child: Text(
-                              'text $i',
-                              style: TextStyle(fontSize: 16.0),
-                            )),
-                      ),
-                    );
-                  },
-                );
-              }).toList(),
-            )
-          ],
+                    },
+                  );
+                }).toList(),
+              )
+            ],
+          ),
         ),
-      ),
-    );
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        floatingActionButton: FloatingActionButton.extended(
+            backgroundColor: ProjectTheme.primaryColor,
+            label: Text(
+              "Add Event",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.w700),
+            ),
+            icon: Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            elevation: 10,
+            isExtended: true,
+            // child: Row(
+            //   children: [Icon(Icons.add), Text("Add Event")],
+            // ),
+
+            onPressed: () {}));
   }
 }
--- lib/app/modules/profile/ProfileView.dart
+++ lib/app/modules/profile/ProfileView.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import '../../core/BaseView.dart';
 import 'ProfileViewModel.dart';
 
@@ -15,8 +16,232 @@ class ProfileView extends BaseView<ProfileViewModel> {
 
   @override
   Widget body(BuildContext context) {
-    return const Scaffold(
-      body: Column(),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            const CircleAvatar(
+              backgroundImage: AssetImage("assets/images/user1.png"),
+              backgroundColor: Colors.white,
+              maxRadius: 50,
+              minRadius: 40,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Card(
+              surfaceTintColor: Colors.grey.shade50,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    controller.user.value.Name,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("${controller.user.value.Age} Years Old",
+                          style: TextStyle(
+                              fontSize: 12, fontWeight: FontWeight.w300)),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Icon(
+                        Icons.circle,
+                        size: 4,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        "${controller.user.value.Gender} ",
+                        style: TextStyle(
+                            fontSize: 12, fontWeight: FontWeight.w300),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    textAlign: TextAlign.center,
+                    "${controller.user.value.Address}",
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.w300),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Padding(
+                //   padding: const EdgeInsets.all(12.0),
+                //   child: SizedBox(
+                //       width: MediaQuery.of(context).size.width,
+                //       child: const Row(
+                //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                //           children: [
+                //             Text("View My Profile"),
+                //             Icon(
+                //               Icons.edit,
+                //               size: 16,
+                //             )
+                //           ])),
+                // ),
+                const SizedBox(
+                  height: 12,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 12.0),
+                  child: Text(
+                    "Account Settings",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Card(
+                  surfaceTintColor: Colors.grey.shade50,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 12,
+                      ),
+                      SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: const Row(children: [
+                            Expanded(
+                              flex: 1,
+                              child: Icon(
+                                Icons.credit_card,
+                                size: 22,
+                              ),
+                            ),
+                            Expanded(flex: 8, child: Text("Payment Methods")),
+                            Expanded(
+                              flex: 1,
+                              child: Icon(
+                                Icons.arrow_forward_ios,
+                                size: 16,
+                              ),
+                            )
+                          ])),
+                      SizedBox(
+                        height: 14,
+                      ),
+                      SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: const Row(children: [
+                            Expanded(
+                              flex: 1,
+                              child: Icon(
+                                Icons.notifications_outlined,
+                                size: 22,
+                              ),
+                            ),
+                            Expanded(flex: 8, child: Text("Notification")),
+                            Expanded(
+                              flex: 1,
+                              child: Icon(
+                                Icons.arrow_forward_ios,
+                                size: 16,
+                              ),
+                            )
+                          ])),
+                      SizedBox(
+                        height: 12,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                const Padding(
+                  padding: const EdgeInsets.only(left: 12.0),
+                  child: Text(
+                    "Legal",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Card(
+                  surfaceTintColor: Colors.grey.shade50,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 12,
+                      ),
+                      SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: const Row(children: [
+                            Expanded(
+                              flex: 1,
+                              child: Icon(
+                                Icons.feed_outlined,
+                                size: 22,
+                              ),
+                            ),
+                            Expanded(
+                                flex: 8, child: Text("Terms and Services")),
+                            Expanded(
+                              flex: 1,
+                              child: Icon(
+                                Icons.arrow_forward_ios,
+                                size: 16,
+                              ),
+                            )
+                          ])),
+                      SizedBox(
+                        height: 14,
+                      ),
+                      SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: const Row(children: [
+                            Expanded(
+                              flex: 1,
+                              child: Icon(
+                                Icons.feed_outlined,
+                                size: 22,
+                              ),
+                            ),
+                            Expanded(
+                                flex: 8, child: Text("Privacy and Privacy")),
+                            Expanded(
+                              flex: 1,
+                              child: Icon(
+                                Icons.arrow_forward_ios,
+                                size: 16,
+                              ),
+                            )
+                          ])),
+                      SizedBox(
+                        height: 12,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
